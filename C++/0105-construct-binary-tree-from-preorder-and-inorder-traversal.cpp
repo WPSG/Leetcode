ad487f27b87be62fb5f2ec8b2e82491bc6e4c2ab
ROC <roc.w@foxmail.com>
2021-01-18 15:52:51 +0800

    0105 Construct Binary Tree from Preorder and Traversal fix #13

---
+++ C++/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,62 @@
+/*
+ * @Date: 2021-01-18 14:39:39
+ * @LastEditors: ROC
+ * @LastEditTime: 2021-01-18 15:44:15
+ * @FilePath: \Leetcode\C++\0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+ */
+
+#include <cstdio>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* myBuildTree(vector<int>& preorder, vector<int>& inorder, int preorder_left, 
+                          int preorder_right, int inorder_left, int inorder_right, unordered_map<int, int> &index){
+      if(preorder_left > preorder_right) return nullptr;
+      // 前序遍历中的第一个节点就是根节点
+      int preorder_root = preorder_left;
+      // 在中序遍历中定位根节点
+      int inorder_root = index[preorder[preorder_root]];
+      
+      // 先把根节点建立出来
+      TreeNode* root = new TreeNode(preorder[preorder_root]);
+      // 得到左子树中的节点数目
+      int size_left_subtree = inorder_root - inorder_left;
+      // 递归地构造左子树，并连接到根节点
+      // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+      root->left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1, index);
+      // 递归地构造右子树，并连接到根节点
+      // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+      root->right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right, index);
+      return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+      int n = preorder.size();
+      unordered_map<int, int> index;
+      // 构造哈希映射，帮助我们快速定位根节点
+      for (int i = 0; i < n; ++i) {
+        index[inorder[i]] = i;
+      }
+      return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1,index);
+    }
+};
+int main(){
+  vector<int> preorder = {3,9,20,15,7};
+  vector<int> inorder = {9,3,15,20,7};
+  TreeNode *root = (new Solution)->buildTree(preorder,inorder);
+  while(root){
+    printf("%d ", root->val);
+    root = root->right;
+  }
+}
