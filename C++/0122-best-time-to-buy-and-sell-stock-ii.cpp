74fcf1a64c344c880b8b5b104c294d2340d40db5
ROC <roc.w@foxmail.com>
2021-05-25 21:31:37 +0800

    add solution using DPTable 0122

--- C++/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ C++/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,12 @@
 /*
  * @Date: 2021-01-19 15:28:06
  * @LastEditors: ROC
- * @LastEditTime: 2021-01-19 16:23:27
+ * @LastEditTime: 2021-05-25 21:28:20
  * @FilePath: \Leetcode\C++\0122-best-time-to-buy-and-sell-stock-ii.cpp
  */
 #include <cstdio>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
@@ -19,10 +20,26 @@ public:
     }
     return profit;
   }
+
+  int maxProfitDP(vector<int> &prices) {
+    int n = prices.size();
+    // dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i])
+    // dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i])
+    // k 是无限大，所以 k-1 也为无限大，化简为：
+    // dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+    // dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    int dp_i_0 = 0, dp_i_1 = INT_MIN;
+    for(int i = 0; i < n; i++) {
+      int pre = dp_i_0;
+      dp_i_0 = max(dp_i_0, dp_i_1 + prices[i]);
+      dp_i_1 = max(dp_i_1, pre - prices[i]);
+    }
+    return dp_i_0;
+  }
 };
 
 int main(){
   vector<int> prices = {7,1,5,3,6,4};
-  int res = (new Solution)->maxProfit(prices);
+  int res = (new Solution)->maxProfitDP(prices);
   printf("%d", res);
 }
